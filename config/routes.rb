c292c109e5faafe89972a23b76a721ad50d5d3eb
Matt Kragen <mattdkragen@gmail.com>
2021-09-24 19:30:43 -0700

    Perform initial gem installation and RSpec setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :forecast
+      resources :backgrounds
+      resources :users
+      resources :sessions
+      resources :road_trip
+    end
+  end
 end

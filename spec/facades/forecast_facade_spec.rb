9ae198344612877d8553b067bb43e06a46308d23
Matt Kragen <mattdkragen@gmail.com>
2021-09-26 12:09:00 -0700

    Test Forecast facade

---
+++ spec/facades/forecast_facade_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe ForecastFacade do
+  describe 'forecast creation' do
+    it 'creates Forecast object', :vcr do
+      location = GeocodeFacade.retrieve_geocode("denver,co")
+      forecast = ForecastFacade.get_forecast(location)
+      expect(forecast).to be_a(Forecast)
+    end
+  end
+end

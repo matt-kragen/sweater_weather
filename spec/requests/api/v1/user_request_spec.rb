d88c5d63d41b68556518f20618de8769d01273d4
Matt Kragen <mattdkragen@gmail.com>
2021-09-27 16:13:49 -0700

    Test user creation happy path

---
+++ spec/requests/api/v1/user_request_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe 'User requests' do
+  describe 'User creation' do
+    it 'can create user and return JSON response' do
+      headers = { 'Accept' => 'application/json', 'Content-Type' => 'application/json'}
+      params = {
+        "email": "test@test.com",
+        "password": "fhtagn",
+        "password_confirmation": "fhtagn"
+      }
+      expect(User.count).to eq(0)
+      
+      post api_v1_users_path, headers: headers, params: params.to_json
+
+      expect(response).to be_successful
+      expect(response.status).to eq(201)
+      parsed_response = JSON.parse(response.body, symbolize_names: true)
+      
+      expect(parsed_response).to be_a(Hash)
+      expect(parsed_response[:data]).to be_a(Hash)
+      expect(parsed_response[:data].keys).to eq([:id, :type, :attributes])
+      
+      expect(parsed_response[:data][:type]).to eq('user')
+      expect(parsed_response[:data][:id]).to be_a(String)
+      expect(parsed_response[:data][:attributes]).to be_a(Hash)
+      
+      expect(parsed_response[:data][:attributes].keys).to eq([:email, :api_key])
+      expect(parsed_response[:data][:attributes][:email]).to eq('test@test.com')
+      expect(parsed_response[:data][:attributes][:api_key]).to be_a(String)
+      
+      expect(User.count).to eq(1)
+    end
+  end
+end

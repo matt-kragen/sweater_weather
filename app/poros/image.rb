8e36f41a5547cc09cd25f5fadf4bc2e0e9e698b8
Matt Kragen <mattdkragen@gmail.com>
2021-09-27 13:18:53 -0700

    Correct lack of comma

--- app/poros/image.rb
+++ app/poros/image.rb
@@ -5,9 +5,9 @@ class Image
     @url = data[:photos][0][:url]
     @images = data[:photos][0][:src]
     @credits = {
-      photographer: data[:photos][0][:photographer]
+      photographer: data[:photos][0][:photographer],
       pexels_credit: 'Photo provided by Pexels',
-      pexels_link: 'https://www.pexels.com',
+      pexels_link: 'https://www.pexels.com'
     }
   end
 end

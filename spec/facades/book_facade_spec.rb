234e12c5eda60d0a25a47b0ff7f0b08da570fcb5
Matt Kragen <mattdkragen@gmail.com>
2021-09-27 10:19:47 -0700

    Pass book facade spec

--- spec/facades/book_facade_spec.rb
+++ spec/facades/book_facade_spec.rb
@@ -6,11 +6,9 @@ RSpec.describe BookFacade do
       location = "denver,co"
       coords = GeocodeFacade.retrieve_geocode(location)
       forecast = ForecastFacade.get_forecast(coords)
-
       quantity = 1
       
       book_query = BookFacade.get_books(location, quantity, coords)
-      require 'pry'; binding.pry
       expect(book_query).to be_a(BookQuery)
     end
   end

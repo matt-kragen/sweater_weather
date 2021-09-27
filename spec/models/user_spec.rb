8c8c675c88e5524b2b80776f5d05fa0d02f9fa7c
Matt Kragen <mattdkragen@gmail.com>
2021-09-27 15:39:18 -0700

    Test User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,12 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'validations' do
-    it { should validate_presence_of(:email)}
-    it { should validate_uniqueness_of(:email)}
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+    it { should have_secure_password(:password) }
+    it { should validate_confirmation_of(:password) }
+    it { should validate_confirmation_of(:password) }
+    it { should validate_presence_of(:password_confirmation) }
+    it { should have_secure_token(:api_key) }
   end
 end

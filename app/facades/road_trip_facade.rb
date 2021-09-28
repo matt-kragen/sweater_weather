86519eee7cf17e2af59ba91f8bb4e74953d25509
Matt Kragen <mattdkragen@gmail.com>
2021-09-28 16:00:47 -0700

    Test facade function

---
+++ app/facades/road_trip_facade.rb
@@ -0,0 +1,8 @@
+class RoadTripFacade
+  def self.get_route(locations)
+    coords = GeocodeFacade.retrieve_geocode(locations[:destination])
+    route = RoadTripService.call_route(locations)
+    forecast = ForecastService.call_forecast(coords)
+    RoadTrip.new(locations, route, forecast)
+  end
+end
---
+++ spec/facades/road_trip_facade_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe RoadTripFacade do
+  describe 'roadtrip creation' do
+    it 'creates RoadTrip object', :vcr do
+      params = { origin: 'denver,co', destination: 'pueblo,co' }
+      road_trip = RoadTripFacade.get_route(params)
+      
+      expect(road_trip).to be_a(RoadTrip)
+      expect(road_trip.start_city).to eq('Denver,Co')
+      expect(road_trip.end_city).to eq('Pueblo,Co')
+      expect(road_trip.travel_time).to be_a(String)
+      expect(road_trip.weather_at_eta).to be_a(Hash)
+      expect(road_trip.weather_at_eta[:temperature]).to be_a(Numeric)
+      expect(road_trip.weather_at_eta[:conditions]).to be_a(String)
+    end
+  end
+end

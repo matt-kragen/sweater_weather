814dfcedc9afeda4abd9f2c2bf5e48114d088824
Matt Kragen <mattdkragen@gmail.com>
2021-09-28 14:16:55 -0700

    Test sad path

--- app/services/road_trip_service.rb
+++ app/services/road_trip_service.rb
@@ -1,9 +1,9 @@
 class RoadTripService < BaseService
-  def self.call_route(route)
+  def self.call_route(locations)
     response = conn('https://www.mapquestapi.com/directions/v2/route').get do |req|
       req.params[:key] = ENV['MAPQUEST_KEY']
-      req.params[:from] = route[:origin]
-      req.params[:to] = route[:destination]
+      req.params[:from] = locations[:origin]
+      req.params[:to] = locations[:destination]
     end
     parse(response)
   end
--- spec/services/road_trip_service_spec.rb
+++ spec/services/road_trip_service_spec.rb
@@ -9,20 +9,41 @@ RSpec.describe RoadTripService do
   end
 
   describe 'class methods' do
-    it '::call_route', :vcr do
-      params = { origin: 'denver,co', destination: 'pueblo,co' }
-      route_response = RoadTripService.call_route(params)
+    describe '::call_route' do
+      describe 'happy path' do
+        it 'retrieves a route if possible', :vcr do
+          params = { origin: 'denver,co', destination: 'pueblo,co' }
+          route_response = RoadTripService.call_route(params)
+    
+          expect(route_response).to be_a(Hash)
+          expect(route_response.keys).to eq([:route, :info])
+           
+          expect(route_response[:route]).to be_a(Hash)
+          expect(route_response[:route]).to have_key(:formattedTime)
+          expect(route_response[:route][:formattedTime]).to be_a(String) 
+           
+          expect(route_response[:info]).to be_a(Hash)
+          expect(route_response[:info]).to have_key(:statuscode)
+          expect(route_response[:info][:statuscode]).to eq(0)
+        end
+      end
 
-      expect(route_response).to be_a(Hash)
-      expect(route_response.keys).to eq([:route, :info])
-       
-      expect(route_response[:route]).to be_a(Hash)
-      expect(route_response[:route]).to have_key(:formattedTime)
-      expect(route_response[:route][:formattedTime]).to be_a(String) 
-       
-      expect(route_response[:info]).to be_a(Hash)
-      expect(route_response[:info]).to have_key(:statuscode)
-      expect(route_response[:info][:statuscode]).to eq(0)
+      describe 'sad path' do
+        it 'returns different status code if route not possible', :vcr do
+          params = { origin: 'denver,co', destination: 'honolulu' }
+          route_response = RoadTripService.call_route(params)
+    
+          expect(route_response).to be_a(Hash)
+          expect(route_response.keys).to eq([:route, :info])
+           
+          expect(route_response[:route]).to be_a(Hash)
+          expect(route_response[:route]).to_not have_key(:formattedTime)
+           
+          expect(route_response[:info]).to be_a(Hash)
+          expect(route_response[:info]).to have_key(:statuscode)
+          expect(route_response[:info][:statuscode]).to_not eq(0)
+        end
+      end
     end
   end
 end

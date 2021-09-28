fa0e6cb1c05d61f828d8ee75aea381df3bad53fc
Matt Kragen <mattdkragen@gmail.com>
2021-09-27 18:35:01 -0700

    Implement sessions controller

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: params[:email])
+      .try(:authenticate, params[:password])
+    
+    if user
+      session[:user_id] = user.id
+      render json: UserSerializer.new(user), status: :ok
+    else
+      render status: :unauthorized
+    end
+  end
+end
---
+++ spec/requests/api/v1/session_request_spec.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+
+describe 'Session requests' do
+  describe 'Session creation' do
+    describe 'happy path' do
+      it 'successfully creates session and displays appropriate response' do
+        user_params = {
+          email: 'test@test.com',
+          password: 'fhtagn',
+          password_confirmation: 'fhtagn'
+        }
+        User.create(user_params)
+  
+        headers = { 'Accept' => 'application/json', 'Content-Type' => 'application/json'}
+        params = {
+          "email": "test@test.com",
+          "password": "fhtagn"
+        }
+        post api_v1_sessions_path, headers: headers, params: params.to_json
+  
+        expect(response).to be_successful
+        expect(response.status).to eq(200)
+        parsed_response = JSON.parse(response.body, symbolize_names: true)
+  
+        expect(parsed_response).to be_a(Hash)
+        expect(parsed_response).to have_key(:data)
+        expect(parsed_response[:data]).to be_a(Hash)
+  
+        expect(parsed_response[:data].keys).to eq([:id, :type, :attributes])
+        expect(parsed_response[:data][:id]).to be_a(String)
+        expect(parsed_response[:data][:type]).to eq('user')
+        expect(parsed_response[:data][:attributes]).to be_a(Hash)
+        
+        expect(parsed_response[:data][:attributes].keys).to eq([:email, :api_key])
+        expect(parsed_response[:data][:attributes][:email]).to be_a(String)
+        expect(parsed_response[:data][:attributes][:api_key]).to be_a(String)
+      end
+    end
+
+    describe 'sad path' do
+      xit 'displays 401 error' do
+        user_params = {
+          email: 'test@test.com',
+          password: 'fhtagn',
+          password_confirmation: 'fhtagn'
+        }
+        User.create(user_params)
+  
+        headers = { 'Accept' => 'application/json', 'Content-Type' => 'application/json'}
+        params = {
+          "email": "test@test.com",
+          "password": "fhtag"
+        }
+
+        post api_v1_sessions_path, headers: headers, params: params.to_json
+  
+        expect(response).to_not be_successful
+        expect(response.status).to eq(401)
+
+        expect(response.body).to eq()
+      end
+    end
+  end
+end
